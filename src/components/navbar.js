3ed19e5fbc417ab780223ea16b2168881ba88c05
Larissa Chen <larissachen13@gmail.com>
2016-09-03 01:08:51 -0400

    css slide out

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,25 @@
 import React from 'react';
 // import { Link } from 'react-router';
 import '../style.scss';
+import { $ } from 'jquery';
 
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+  scroll(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $('#portfolios').offset().top,
+    }, 2000);
+  }
 
   render() {
     return (
       <div className="navbarContainer">
         <div className="name"> Larissa Chen </div>
+        <a rel="" href="#portfolios" onClick={this.scroll}> <i className="fa fa-chevron-down fa-2x" aria-hidden="true"></i> </a>
       </div>
     );
   }
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Portfolio from './portfolio';
 
+const handleClick = (event) => {
+  event.target.classList.toggle('active');
+  event.target.parentElement.nextElementSibling.classList.toggle('show');
+};
+
 // function based "dumb" component with no state
 const Welcome = () => {
   return (
@@ -10,10 +15,14 @@ const Welcome = () => {
           <p id="first"> Software and Web Development and Design </p>
           <p id="middle"> Dartmouth Junior</p>
           <p id="last"> CS Major, Double Minor in Physics and Spanish </p>
+          <i id="about-more-2" className="fa fa-2x fa-caret-right accordion" aria-hidden="true" onClick={handleClick}></i>
+        </div>
+        <div className="about-site panel">
+          <p> This site is built with React, CSS, and javascript </p>
         </div>
       </div>
       <h4 className="portfolio-header"> Portfolio </h4>
-      <Portfolio />
+      <Portfolio id="portfolios" />
       <footer>
         <span> Sept 2016 </span>
         <i className="fa fa-heart-o" aria-hidden="true"></i>
